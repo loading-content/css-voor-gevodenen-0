c10f0d3e1845c6791b64dd2a572c9c258c486fce
loading-content <56720934+loading-content@users.noreply.github.com>
2023-02-02 11:55:56 +0100

    Add files via upload

--- scripts/detect.js
+++ scripts/detect.js
@@ -1,5 +1,14 @@
 if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
   document.write("mobile");
-}else{
-  document.write("not mobile");
 }
+else{
+  document.write("not mobile");
+  about = document.getElementById("yes");
+  about.style.color = 'blue';
+}
+
+var btn = document.getElementById('btn')
+
+btn.addEventListener('click', function(){
+   alert('hello');
+});
