1d3d00a99b441f3d4757684b1956d4e03a12fc23
loading-content <56720934+loading-content@users.noreply.github.com>
2023-02-02 12:38:54 +0100

    Add files via upload

--- scripts/detect.js
+++ scripts/detect.js
@@ -8,10 +8,10 @@ btn.addEventListener('click', butt);
 function butt() {
   if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
     ss.style.background = 'radial-gradient(circle, red, black';
-    devise.innerHTML = devise.innerHTML + "mobile" + " device";
+    devise.innerHTML = "youre on a: mobile device";
   }
   else{
     ss.style.background = 'radial-gradient(circle, blue, pink';
-    devise.innerHTML = devise.innerHTML + "not mobile" + " device";
+    devise.innerHTML = "youre on a: not mobile device";
   }
 }
